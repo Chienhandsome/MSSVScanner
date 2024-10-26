57ce6dd7adc212eaa77941bf40b3bcf29e27c0a6
LackOfIntelligentGuy <viethuu04sg@gmail.com>
2024-10-26 19:35:27 +0700

    Add QueueListAdpater & decorate recyclerView

--- app/src/main/java/com/zeeshanelahi/barcodescannerandcameraxdemo/view/fragment/QueueListFragment.java
+++ app/src/main/java/com/zeeshanelahi/barcodescannerandcameraxdemo/view/fragment/QueueListFragment.java
@@ -1,29 +1,53 @@
 package com.zeeshanelahi.barcodescannerandcameraxdemo.view.fragment;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.InsetDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.zeeshanelahi.barcodescannerandcameraxdemo.R;
+import com.zeeshanelahi.barcodescannerandcameraxdemo.databinding.FragmentQueueListBinding;
+
+import java.util.ArrayList;
 
 public class QueueListFragment extends Fragment {
+    private FragmentQueueListBinding fragmentBinding;
+    private ArrayList<String> mssvList;
+    private QueueListAdapter queueListAdapter;
     public QueueListFragment() {
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mssvList = new ArrayList<>();
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_queue_list, container, false);
+        fragmentBinding = FragmentQueueListBinding.inflate(inflater, container, false);
+        View view = fragmentBinding.getRoot();
+        queueListAdapter = new QueueListAdapter(mssvList);
+        fragmentBinding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        Drawable oDrawable = ContextCompat.getDrawable(getContext(), R.drawable.divider_line);
+        InsetDrawable insetDrawable = new InsetDrawable(oDrawable, 0, 10, 0, 0);
+        //
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL);
+        dividerItemDecoration.setDrawable(insetDrawable);
+        fragmentBinding.recyclerView.addItemDecoration(dividerItemDecoration);
+        fragmentBinding.recyclerView.setAdapter(queueListAdapter);
         setUpViewEvents();
         return view;
     }
